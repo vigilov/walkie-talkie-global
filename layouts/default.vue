4f57158b172bd527b28c31138e1a147752950500
lexis <lexis@trafficstars.com>
2023-11-25 15:53:56 +0200

    user profile popup

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 
 import {useAuth} from "~/composables/useAuth";
 
-const {user, signOut} = useAuth()
+const {user, signIn, signOut} = useAuth()
 
 </script>
 
@@ -14,15 +14,41 @@ const {user, signOut} = useAuth()
       </div>
       <div class="user">
         <ClientOnly>
-          <NuxtLink v-if="user" to="/profile" class="profile">
-            <div class="displayName">{{ user.displayName }}</div>
-            <img
-                :src="<string>user.photoURL"
-                alt="Profile"
-                class="rounded-full w-10 h-10 border"
+          <HeadlessMenu v-if="user" as="div" class="relative inline-block text-left">
+            <HeadlessMenuButton as="div" class="profile">
+              <span>{{ user.displayName }}</span>
+              <img
+                  :src="<string>user.photoURL"
+                  alt="Profile"
+                  class="rounded-full w-10 h-10 border"
+              />
+            </HeadlessMenuButton>
+
+            <transition
+                enter-active-class="transition duration-100 ease-out"
+                enter-from-class="transform scale-95 opacity-0"
+                enter-to-class="transform scale-100 opacity-100"
+                leave-active-class="transition duration-75 ease-out"
+                leave-from-class="transform scale-100 opacity-100"
+                leave-to-class="transform scale-95 opacity-0"
             />
-          </NuxtLink>
-          <NuxtLink v-else to="/auth/sign-in" class="button second">Sign In</NuxtLink>
+
+            <HeadlessMenuItems class="profile-items">
+              <HeadlessMenuItems>
+                <HeadlessMenuItem v-slot="{ active }">
+                  <a :class='{ "active": active }' class="group profile-item" href="/events/user">My
+                    Events</a>
+                </HeadlessMenuItem>
+                <HeadlessMenuItem v-slot="{ active }">
+                  <a :class='{ "active": active }' class="group profile-item" href="/users/">Profile</a>
+                </HeadlessMenuItem>
+                <HeadlessMenuItem v-slot="{ active }">
+                  <a :class='{ "active": active }' class="group profile-item" @click="signOut">Logout</a>
+                </HeadlessMenuItem>
+              </HeadlessMenuItems>
+            </HeadlessMenuItems>
+          </HeadlessMenu>
+          <div v-else @click="signIn" class="button second">Sign In</div>
         </ClientOnly>
       </div>
       <div class="create-event button primary">
@@ -41,7 +67,7 @@ const {user, signOut} = useAuth()
   </div>
 </template>
 
-<style scoped lang="sass">
+<style lang="sass" scoped>
 .header
   @apply flex items-center justify-between
   width: 1440px
@@ -53,11 +79,37 @@ const {user, signOut} = useAuth()
 .user
   padding-right: 24px
 
-  .profile
-    @apply flex items-center
-    gap: 12px
+.profile
+  @apply flex items-center
+  @apply cursor-pointer
+  gap: 12px
+  text-align: center
+  font-feature-settings: 'clig' off, 'liga' off
+  font-family: Corsa Grotesk, sans-serif
+  font-size: 16px
+  font-style: normal
+  font-weight: 400
+  line-height: 20px
+
+
+.profile-items
+  @apply absolute right-0 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none
+
+.profile-item
+  @apply flex w-full items-center rounded-md
+  display: flex
+  width: 210px
+  padding: 8px 20px
+  align-items: center
+  gap: 10px
+  &.active
+    background: #d18dd7
 
 .title
-  font-weight: bold
+  font-family: Affect, sans-serif
+  font-size: 20px
+  font-style: normal
+  font-weight: 400
+  line-height: normal
 
 </style>
---
+++ pages/users/[id].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="header">
+    <div class="back">
+      <Icon name="arrow-left"/>
+      Go back to all events
+    </div>
+  </div>
+</template>
+
+<style scoped lang="sass">
+</style>
